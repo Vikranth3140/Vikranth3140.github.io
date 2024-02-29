9defca79f7f7e1bfaffb9fc9737da021d81fe3f0
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-02-29 20:17:46 +0530

    Refactoreed the entire code

---
+++ README.md
@@ -0,0 +1,39 @@
+# My Portfolio
+
+Welcome to my portfolio website, crafted with HTML, CSS, Bootstrap, JavaScript, Boxicons, ScrollReveal, and Typed.js.
+
+Check it out at [Vikranth3140](https://vikranth3140.github.io/)
+
+This website showcases my projects, skills, and how to get in touch. Here's what you'll find:
+
+- Home: A brief introduction and links to my social media profiles.
+- About: Learn more about my background, interests, and experiences.
+- Skills: Discover the programming languages, tools, and technologies I'm proficient in.
+- Portfolio: Dive into my projects with descriptions and links to their GitHub - repositories.
+- Contact: A convenient form for visitors to send messages directly to me.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Typed.js, ScrollReveal, Boxicons
+
+## Setup
+
+1. Clone the repository: 
+    ```bash
+    git clone https://github.com/Vikranth3140/Vikranth3140.github.io
+    ```
+
+2. Open `index.html` in a web browser to view the website locally.
+
+## Usage
+
+Feel free to fork this repository to create your own portfolio website!
+
+If you decide to use this template, please provide attribution by linking back to this repository.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ script.js
@@ -0,0 +1,62 @@
+// Selecting the menu icon and navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+// Toggling the menu icon and navbar on click
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// Selecting all sections and navigation links
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+// Handling scroll event
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        // Adding 'active' class to the navigation link corresponding to the current section
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // Toggling the 'sticky' class on the header based on scroll position
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.screenY > 100);
+
+    // Resetting the menu icon and navbar
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+// Initializing ScrollReveal library
+ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+// Revealing elements with specified origin
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+// Initializing Typed.js library
+const typed = new Typed('.multiple-text', {
+    strings: ['CSAI Sophomore at IIITD', 'Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
