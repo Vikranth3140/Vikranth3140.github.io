3c8b5bfba4e3a3a9f087f9f13f1c25bfe9849345
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-06-20 03:47:04 +0530

    Sophomore --> Junior

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // Initializing Typed.js library
 const typed = new Typed('.multiple-text', {
-    strings: ['CSAI Sophomore at IIITD', 'Developer'],
+    strings: ['CSAI Junior at IIITD', 'Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
